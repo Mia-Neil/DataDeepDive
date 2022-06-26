0faeed7b5b2ab21ca009772e96bebbadfb82bbd8
Mia Neil <mia.e.atkinson@gmail.com>
2022-06-26 00:17:00 -0400

    running scraping tool

--- Leotard_Bonus/rtn_scrape.py
+++ Leotard_Bonus/rtn_scrape.py
@@ -2,8 +2,9 @@ import csv
 import requests
 from datetime import datetime
 
-years = ['2022']
-
+# years = ['2022', '2021', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009',
+# '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001', '2000', '1999', '1998' ]
+years = ['2022-04']
 list_of_rows = []
 headers = ['Season','Meet ID','Meet Date','Host','Opponents','Meet Name','Gymnast ID','Name','Team','Event','RQS','Score']
 list_of_rows.append(headers)
